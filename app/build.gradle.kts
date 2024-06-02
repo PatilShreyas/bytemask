3fefd6e7d99d6670dcd86fadaf1b0f583a6c3d4f
Shreyas <shreyaspatilg@gmail.com>
2024-06-02 16:12:10 +0530

    Setup sample app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("dev.shreyaspatil.bytemask.plugin")
 }
 
 android {
@@ -33,8 +34,11 @@ android {
         targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions { jvmTarget = "1.8" }
-    buildFeatures { compose = true }
-    composeOptions { kotlinCompilerExtensionVersion = "1.5.12" }
+    buildFeatures {
+        viewBinding {
+            enable = true
+        }
+    }
     packaging { resources { excludes += "/META-INF/{AL2.0,LGPL2.1}" } }
     signingConfigs { create("release") { initWith(getByName("debug")) } }
 }
@@ -42,17 +46,25 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
+    implementation("androidx.activity:activity-ktx:1.8.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+}
+
+bytemaskConfig {
+    // UNCOMMENT THIS: To set the generated class name as "MyAppConfig"
+//    className.set("MyAppConfig")
+
+    // UNCOMMENT THIS: To pick secrets from the `secrets.properties`
+//    defaultPropertiesFileName.set("secrets.properties")
+
+    // For debug variant, enable encryption with a debug SHA-256 key.
+    configure("debug") {
+        enableEncryption.set(true)
+    }
+
+    configure("release") {
+        enableEncryption.set(true)
+    }
 }
--- app/src/main/java/dev/shreyaspatil/bytemask/example/MainActivity.kt
+++ app/src/main/java/dev/shreyaspatil/bytemask/example/MainActivity.kt
@@ -17,41 +17,22 @@ package dev.shreyaspatil.bytemask.example
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import dev.shreyaspatil.bytemask.example.ui.theme.ByteMaskTheme
+import dev.shreyaspatil.bytemask.example.databinding.ActivityMainBinding
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        setContent {
-            ByteMaskTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    //  Greeting(BytemaskConfig.API_KEY)
-                }
-            }
+        with(binding) {
+            buttonApiKey.setOnClickListener { setSecret(BytemaskConfig.API_KEY) }
+            buttonAccessToken.setOnClickListener { setSecret(BytemaskConfig.ACCESS_TOKEN) }
+            buttonApiEndpoint.setOnClickListener { setSecret(BytemaskConfig.API_ENDPOINT) }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(text = "Hello $name!", modifier = modifier)
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ByteMaskTheme { Greeting("Android") }
+    private fun ActivityMainBinding.setSecret(secret: String) {
+        textView.text = secret
+    }
 }
